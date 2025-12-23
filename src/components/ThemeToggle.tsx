23062c0d79eac26ac458797548e0376d129db928
MaxIliv <max.r.iliv@gmail.com>
2025-12-23 18:40:57 +0200

    feat: Custom Icons on theme toggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,15 +1,23 @@
+import { MoonStarIcon, SunDimIcon } from "lucide-react"
 import { useTheme } from "../app/providers/ThemeProvider"
+import { Button } from "./ui/button"
 
 
 export function ThemeToggle() {
   const { theme, toggleTheme } = useTheme()
 
   return (
-    <button
+    <Button
       onClick={toggleTheme}
-      className="rounded-full cursor-pointer px-3 py-2 text-sm"
+      variant='outline'
+      size="icon"
+      className="rounded-full cursor-pointer"
     >
-      {theme === 'light' ? '☀️' : '🌙'}
-    </button>
+      {
+        theme === 'light' ?
+          <SunDimIcon />
+          : <MoonStarIcon />
+      }
+    </Button>
   )
 }
