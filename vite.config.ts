b5673dd62d5ff897e8e501f8c75178acfcdbac41
MaxIliv <max.r.iliv@gmail.com>
2026-01-04 17:25:07 +0200

    chore: Define manual chunks to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
-import path from "path";
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   test: {
     globals: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          recharts: ['recharts'],
+          router: ['react-router'],
+          table: ['@tanstack/react-table'],
+          icons: ['lucide-react'],
+        },
+      },
+    },
   },
   resolve: {
     alias: {
