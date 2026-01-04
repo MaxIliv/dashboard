e9271449b497cabb14e6a38b7690f37e28c74d04
MaxIliv <max.r.iliv@gmail.com>
2026-01-04 16:11:56 +0200

    fix: suspense show on every page change

--- src/pages/users/UsersPage.tsx
+++ src/pages/users/UsersPage.tsx
@@ -1,27 +1,23 @@
-import { Skeleton } from '@/components/ui/skeleton';
 import TableDisplayOptions from '@/features/Users/components/TableDisplayOptions';
 import { TablePagination } from '@/features/Users/components/TablePagination';
 import TableSearch from '@/features/Users/components/TableSearch';
 import TableView from '@/features/Users/components/TableView';
 import { UsersTableProvider } from '@/features/Users/context/users-table.provider';
-import { Suspense } from 'react';
 
 export default function UsersPage() {
   return (
     <section className="w-full grid gap-4">
       <h2>Users</h2>
 
-      <Suspense fallback={<Skeleton className="w-full h-64" />}>
-        <UsersTableProvider>
-          <div className="flex items-center justify-between flex-wrap gap-2">
-            <TableSearch />
-            <TableDisplayOptions />
-          </div>
+      <UsersTableProvider>
+        <div className="flex items-center justify-between flex-wrap gap-2">
+          <TableSearch />
+          <TableDisplayOptions />
+        </div>
 
-          <TableView />
-          <TablePagination />
-        </UsersTableProvider>
-      </Suspense>
+        <TableView />
+        <TablePagination />
+      </UsersTableProvider>
     </section>
   );
 }
