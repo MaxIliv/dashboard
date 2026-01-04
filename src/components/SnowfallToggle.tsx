89f725604d717b81e6b6e88f4a64fd6487fb210e
MaxIliv <max.r.iliv@gmail.com>
2026-01-04 15:02:56 +0200

    refactor: Sidebar Provider

--- src/components/SnowfallToggle.tsx
+++ src/components/SnowfallToggle.tsx
@@ -1,8 +1,8 @@
-import { useSnowfall } from '@/app/providers/AppProvider';
 import { Button } from '@/components/ui/button';
 import { SnowflakeIcon } from 'lucide-react';
 import SimplelTooltip from './SimplelTooltip';
 import { cn } from '@/lib/utils';
+import { useSnowfall } from '@/features/Sidebar/SidebarProvider';
 
 export default function SnowfallToggle() {
   const { toggleSnowfall, isSnowfallEnabled } = useSnowfall();
--- src/features/Sidebar/Sidebar.tsx
+++ src/features/Sidebar/Sidebar.tsx
@@ -8,45 +8,48 @@ import { SidebarButton } from './SidebarButton';
 import { Link } from 'react-router';
 import SnowfallView from '../snowfall/SnowfallView';
 import SnowfallToggle from '@/components/SnowfallToggle';
+import { SidebarProvider } from './SidebarProvider';
 
 export default function Sidebar() {
   const { logout } = useAuthContext();
   const { isSidebarCollapsed } = useAppContext();
 
   return (
-    <aside
-      className={cn('p-2 w-full flex flex-col gap-4 relative bg-sidebar', {
-        'max-w-64': !isSidebarCollapsed,
-        'flex-0': isSidebarCollapsed,
-      })}
-    >
-      <SnowfallView />
+    <SidebarProvider>
+      <aside
+        className={cn('p-2 w-full flex flex-col gap-4 relative bg-sidebar', {
+          'max-w-64': !isSidebarCollapsed,
+          'flex-0': isSidebarCollapsed,
+        })}
+      >
+        <SnowfallView />
 
-      <Link to="/">
-        <Brand />
-      </Link>
+        <Link to="/">
+          <Brand />
+        </Link>
 
-      <nav className="px-2">
-        <ul className="flex flex-col gap-2">
-          {mainMenu.map((link) => (
-            <li key={link.title} title={link.title}>
-              <NavButton {...link} collapsed={isSidebarCollapsed} />
-            </li>
-          ))}
-        </ul>
-      </nav>
+        <nav className="px-2">
+          <ul className="flex flex-col gap-2">
+            {mainMenu.map((link) => (
+              <li key={link.title} title={link.title}>
+                <NavButton {...link} collapsed={isSidebarCollapsed} />
+              </li>
+            ))}
+          </ul>
+        </nav>
 
-      <div className="flex-1"></div>
+        <div className="flex-1"></div>
 
-      <div className="px-2">
-        <SnowfallToggle />
+        <div className="px-2">
+          <SnowfallToggle />
 
-        <SidebarButton
-          {...logoutLink}
-          collapsed={isSidebarCollapsed}
-          onClick={logout}
-        />
-      </div>
-    </aside>
+          <SidebarButton
+            {...logoutLink}
+            collapsed={isSidebarCollapsed}
+            onClick={logout}
+          />
+        </div>
+      </aside>
+    </SidebarProvider>
   );
 }
