db0bd62e9b0fddef2697cefafb9a1d03491cb278
MaxIliv <max.r.iliv@gmail.com>
2026-01-05 13:06:28 +0200

    feat: emphasise user role

--- src/features/Users/columns.tsx
+++ src/features/Users/columns.tsx
@@ -1,6 +1,13 @@
 import { createColumnHelper } from '@tanstack/react-table';
 import type { User } from './types';
 import { formatDate } from './utils/formatDate';
+import { Badge } from '@/components/ui/badge';
+
+const ROLE_BADGE_VARIANTS = {
+  admin: 'success',
+  moderator: 'secondary',
+  user: 'outline',
+} as const;
 
 const columnHelper = createColumnHelper<User>();
 
@@ -28,11 +35,22 @@ export const columns = [
   }),
   columnHelper.accessor('role', {
     header: 'Role',
+    cell: ({ getValue }) => {
+      const value = getValue();
+
+      return (
+        <div className="lowercase">
+          <Badge variant={ROLE_BADGE_VARIANTS[value]}>{value}</Badge>
+        </div>
+      );
+    },
   }),
   columnHelper.accessor('height', {
     header: 'Height',
+    cell: ({ getValue }) => <div className="lowercase">{getValue()} cm</div>,
   }),
   columnHelper.accessor('weight', {
     header: 'Weight',
+    cell: ({ getValue }) => <div className="lowercase">{getValue()} kg</div>,
   }),
 ];
